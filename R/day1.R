17c67a5f9a07b1f89e48b148395c463e47c6281c
pauljo13 <pauljbs1397@gmail.com>
2025-02-24 17:28:35 +0900

    today

--- R/day1.R
+++ R/day1.R
@@ -15,9 +15,6 @@ library(splines)
 
 # data load
 ges <- getGEO("GSE15824")
-ges[[1]]
-type(ges[[1]])
-attr(ges[[1]], "phenoData")
 pheno <- attr(attr(ges[[1]], "phenoData"), "data")
 exprs <- attr(ges[[1]], "assayData")[["exprs"]]
 
@@ -29,6 +26,13 @@ colnames(exprs)
 colnames(exprs) <- pheno[colnames(exprs),"title"]
 colnames(exprs)
 
+rownames(exprs)
+nan_count <- sum(is.na(rownames(exprs)) | rownames(exprs) == "NaN")
+print(nan_count)
+
+setwd("/Users/knu_cgl1/Desktop/Study/repositories/GSE15824")
+write.table(exprs, file = "GSE15824_expression_original.txt", sep = "\t")
+
 ges[[1]]
 gpl <- getGEO("GPL570")
 gpl_table <- Table(gpl)
@@ -36,12 +40,29 @@ colnames(gpl_table)
 gpl_table["Gene Symbol"]
 gpl_table["ID"]
 
-gene_symbols <- gpl_table$`Gene Symbol`[match(rownames(exprs), gpl_table$ID)]
-colnames(exprs)
+
+original_rownames <- rownames(exprs)
+length(original_rownames)
+
+matched_indices <- match(original_rownames, gpl_table$ID)
+length(matched_indices)
+gene_symbols <- gpl_table$`Gene Symbol`[matched_indices]
+length((gene_symbols))
+
+# 매칭되지 않은 값들 확인
+unmatched_indices <- is.na(gene_symbols)  # NA인 값 찾기
+unmatched_genes <- original_rownames[unmatched_indices]  # 매칭되지 않은 원래 rownames 저장
+
+# NA가 있는 경우, 원래의 rownames 사용
+gene_symbols[unmatched_indices] <- original_rownames[unmatched_indices]
+
+# rownames 변경
 rownames(exprs) <- gene_symbols
-rownames(exprs)
+
+
 
 getwd()
 setwd("/Users/knu_cgl1/Desktop/Study/repositories/GSE15824")
 write.table(exprs, file = "GSE15824_expression.txt", sep = "\t")
 write.table(pheno, file = "GSE15824_phenotype.txt", sep = "\t")
+write.table(gpl_table, file = "GPL570.txt", sep = "\t")
--- README.md
+++ README.md
@@ -5,4 +5,9 @@
 
 Below is a summary of my daily learning milestones:
 - Day 1
-    - 
+    - data load, data EDA
+    - [R](R/day1.R)
+    - [Python](Python/EDA.ipynb)
+- Day 2
+    - EDA
+    - [Python](Python/EDA.ipynb)
