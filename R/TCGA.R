66be6e816a5cd3a60d6c8187b9ba65066aaffbd6
pauljo13 <pauljbs1397@gmail.com>
2025-03-04 17:51:00 +0900

    03-04

--- R/TCGA.R
+++ R/TCGA.R
@@ -43,11 +43,59 @@ print(metadata$definition)
 length(colnames(metadata))
 print(table(metadata$definition))
 
-group <- ifelse(metadata$definition %in% c("Primary solid Tumor", "Recurrent Solid Tumor"), "Tumor", "Normal")
+group <- factor(ifelse(metadata$definition %in% c("Primary solid Tumor", "Recurrent Solid Tumor"), "Tumor", "Normal"))
 print(table(group))
 metadata$group <- group
+
+class(metadata$group)
+str(metadata$group)
+
 
 library(DESeq2)
 dds <- DESeqDataSetFromMatrix(countData = exp_matrix,
                               colData = metadata,
                               design = ~ group)
+print(dds)
+dds <- DESeq(dds)
+res <- results(dds, contrast = c("group", "Tumor", "Normal"))
+View(res)
+
+
+library(ggplot2)
+
+res_df <- as.data.frame(res)
+res_df <- na.omit(res_df)
+res_df$neg_log10_padj <- -log10(res_df$padj)
+
+res_df$significant <- ifelse(abs(res_df$log2FoldChange) > 1 & res_df$padj < 0.05, "Significant", "Not Significant")
+
+
+ggplot(res_df, aes(x = log2FoldChange, y = neg_log10_padj, color = significant)) + 
+  geom_point(alpha = 0.5) +
+  scale_color_manual(values = c("Significant" = "red", "Not Significant" = "grey")) +
+  geom_vline(xintercept = c(-1, 1), linetype = "dashed", color = "black") +  # log2FC 기준선
+  geom_hline(yintercept = -log10(0.05), linetype = "dashed", color = "black") +  # padj 기준선
+  labs(x = "Log2 Fold Change",
+       y = "-Log10 Adjusted P-value",
+       title = "Volcano Plot: Tumor vs Normal",
+       color = "Significance") +
+  theme_minimal()
+
+library(EnhancedVolcano)
+
+# 상위 10개 유전자 선택
+key_genes <- head(res[order(res$padj), ], 10)$log2FoldChange
+names(key_genes) <- head(rownames(res[order(res$padj), ]), 10)
+
+EnhancedVolcano(res,
+                lab = rownames(res),
+                x = "log2FoldChange",
+                y = "padj",
+                selectLab = names(key_genes),  # 라벨링할 유전자
+                title = "Volcano Plot: Tumor vs Normal",
+                pCutoff = 0.05,
+                FCcutoff = 1.0,
+                pointSize = 2.0,
+                labSize = 3.0,
+                drawConnectors = TRUE,         # 라벨과 점 연결선
+                widthConnectors = 0.5)         # 연결선 두께
